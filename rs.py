fe2e0c200bbe370884cb8fdf5281edef894883c9
ahwassa10 <71190521+ahwassa10@users.noreply.github.com>
2022-02-13 17:36:59 -0500

    rs opens and closes a listening socket

--- rs.py
+++ rs.py
@@ -42,7 +42,8 @@ def parseArgs():
     ts2_hostname = arguments[4]
     ts2_port     = parsePort(arguments[5])
 
-def cleanupTSSockets():
+
+def cleanupSockets():
     if ts1_socket != None:
         ts1_socket.shutdown(socket.SHUT_RDWR)
         ts1_socket.close()
@@ -50,6 +51,10 @@ def cleanupTSSockets():
     if ts2_socket != None:
         ts2_socket.shutdown(socket.SHUT_RDWR)
         ts2_socket.close()
+    
+    if rs_socket != None:
+        rs_socket.close()
+
 
 def openTSConnections():
     global ts1_socket
@@ -68,8 +73,21 @@ def openTSConnections():
     
     except OSError as error:
         print(error)
-        cleanupTSSockets()
+        cleanupSockets()
         sys.exit("Error: Unable to connect to ts2 at {} : {}".format(ts2_hostname, ts2_port))
+
+def openListener():
+    global rs_socket
+    try:
+        rs_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        rs_binding = ('', rs_port)
+        rs_socket.bind(rs_binding)
+        rs_socket.listen(3)
+        
+    except OSError as error:
+        print(error)
+        cleanupSockets()
+        sys.exit("Error: Failed to create listening socket")
 
 def main():
     parseArgs()
@@ -80,7 +98,13 @@ def main():
     print("Debug: ts1 at {} : {}".format(ts1_hostname, ts1_port))
     print("Debug: ts2 at {} : {}".format(ts2_hostname, ts2_port))
     
-    cleanupTSSockets()
+    openListener()
+    host = socket.gethostname()
+    ip   = socket.gethostbyname(host)
+    print("Debug: Succesfully opened a listening socket at " \
+          "{} : {} {}".format(host, rs_port, ip))
+    
+    cleanupSockets()
     print("Debug: Succesfully cleaned up connections to ts1 and ts2")
     
 
