0c84e5716cb329c023dd3986ac1c4db841d5957a
ahwassa10 <71190521+ahwassa10@users.noreply.github.com>
2022-02-14 12:20:25 -0500

    RS server implemented

--- rs.py
+++ rs.py
@@ -16,11 +16,11 @@ client_socket   = None # Timeout after CLIENT_TIMEOUT
 
 ts1_port     = 0
 ts1_hostname = ""
-ts1_socket   = None    # Timeout after TS_TIMEOUT
+ts1_response = ""
 
 ts2_port     = 0
 ts2_hostname = ""
-ts2_socket   = None    # Timeout after TS_TIMEOUT
+ts2_response = ""
 
 def parsePort(port):
     if port.isdigit() and int(port) > 0 and int(port) < 65535:
@@ -50,25 +50,13 @@ def parseArgs():
 
 
 def cleanupSockets():
-    if ts1_socket != None:
-        ts1_socket.shutdown(socket.SHUT_RDWR)
-        ts1_socket.close()
-    
-    if ts2_socket != None:
-        ts2_socket.shutdown(socket.SHUT_RDWR)
-        ts2_socket.close()
-    
     # DO NOT call shutdown on a listening socket
     if rs_socket != None:
         rs_socket.close()
-    
-    if client_socket != None:
-        client_socket.shutdown(socket.SHUT_RDWR)
-        client_socket.close()
 
-def queryTS1():
-    global ts1_socket
-    ts1_socket = None
+def queryTS1(data):
+    global ts1_response
+    ts1_response = ""
     
     try:
         ts1_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -77,32 +65,58 @@ def queryTS1():
     
     except OSError as error:
         print(error)
-        sys.exit("Error: Unable to connect to ts1 at {}:{}".format(ts1_hostname, ts1_port))
+        if (ts1_socket != None):
+            ts1_socket.shutdown(socket.SHUT_RDWR)
+            ts1_socket.close()
+        print("    Error: Unable to connect to ts1 at {}:{}".format(ts1_hostname, ts1_port))
+        return
     
-
-
-def openTSConnections():
-    global ts1_socket
     try:
-        ts1_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        ts1_socket.connect((ts1_hostname, ts1_port))
-        ts1_socket.settimeout(TS_TIMEOUT)
+        ts1_socket.send(data.encode("utf-8"))
+        response = ts1_socket.recv(512).decode("utf-8")
+    
+    except socket.timeout:
+        response = ""
     
     except OSError as error:
         print(error)
-        sys.exit("Error: Unable to connect to ts1 at {} : {}".format(ts1_hostname, ts1_port))
+        print("    Error: Major problem occurred when querying TS1")
     
-    global ts2_socket
+    ts1_socket.shutdown(socket.SHUT_RDWR)
+    ts1_socket.close()
+
+def queryTS2(data):
+    global ts2_response
+    ts2_response = ""
+    
     try:
         ts2_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         ts2_socket.connect((ts2_hostname, ts2_port))
-        ts1_socket.settimeout(TS_TIMEOUT)
+        ts2_socket.settimeout(TS_TIMEOUT)
+        
+    except OSError as error:
+        print(error)
+        if (ts2_socket != None):
+            ts2_socket.shutdown(socket.SHUT_RDWR)
+            ts2_socket.close()
+        print("    Error: Unable to connect to ts2 at {}:{}".format(ts2_hostname, ts2_port))
+        return
+    
+    try:
+        ts2_socket.send(data.encode("utf-8"))
+        response = ts2_socket.recv(512).decode("utf-8")
+    
+    except socket.timeout:
+        response = ""
     
     except OSError as error:
         print(error)
-        cleanupSockets()
-        sys.exit("Error: Unable to connect to ts2 at {} : {}".format(ts2_hostname, ts2_port))
-
+        print("    Error: Major problem occurred when querying TS2")
+    
+    ts2_socket.shutdown(socket.SHUT_RDWR)
+    ts2_socket.close()
+        
+    
 def openListener():
     global rs_socket
     try:
@@ -127,7 +141,7 @@ def waitClient():
         client_hostname = address[0]
         client_port     = address[1]
     
-    except socket.timeout
+    except socket.timeout:
         cleanupSockets()
         sys.exit("Error: Listener socket timed out waiting for client")
     
@@ -138,6 +152,7 @@ def waitClient():
 
 
 def readLoop():
+    global client_socket
     client_socket.settimeout(CLIENT_TIMEOUT)
     
     while True:
@@ -146,37 +161,63 @@ def readLoop():
         
         except socket.timeout:
             print("Error: Connection to client timed out")
-            return
+            break
         
         except OSError as error:
             print(error)
-            cleanupSockets()
-            sys.exit("Error: Failed to read from client")
+            print("Error: Failed to read from client")
+            break
         
         data = data.strip()
         if len(data) == 0:
-            return # Client ended the connection
+            break # Client ended the connection
         
         print("Debug: Received from client: {}".format(data))
         
         try:
             
-            t1 = threading.Thread(target=sendTS, name="ts1", args=(data, ts1_socket))
-            t2 = threading.Thread(target=sendTS, name="ts2", args=(data, ts2_socket))
+            t1 = threading.Thread(target=queryTS1, name="ts1", args=(data,))
+            t2 = threading.Thread(target=queryTS2, name="ts2", args=(data,))
             t1.start()
             t2.start()
+            t1.join()
+            t2.join()
             
-            ts1_data = ""
-            ts2_data = ""
-            
-            print("Debug: Received from ts1: {}".format(ts1_data))
-            print("Debug: Received from ts2: {}".format(ts2_data))
+            print("    Debug: Received from ts1: {}".format(ts1_response))
+            print("    Debug: Received from ts2: {}".format(ts2_response))
         
         except OSError as error:
             print(error)
             cleanupSockets()
-            sys.exit("Error: Failed to communicated with ts server")
+            sys.exit("Error: System Error")
+        
+        try:
+            if ts1_response == "" and ts2_response == "":
+                message = "{} - TIMED OUT".format(data)
+                print("    Debug: Sent to client: {}".format(message))
+                client_socket.send(message.encode("utf-8"))
+                
+            elif ts1_response != None and ts2_response != None:
+                print("    ERROR: BOTH TS RESPONDED")
             
+            elif ts1_response != None:
+                client_socket.send(ts1_response.encode("utf-8"))
+            
+            else:
+                client_socket.send(ts2_response.encode("utf-8"))
+        
+        except socket.timeout:
+            print("    Error: Connection to client timed out")
+            break
+        
+        except OSError as error:
+            print(error)
+            print("    Error: Failed to send reponse to client")
+            break
+    
+    client_socket.shutdown(socket.SHUT_RDWR)
+    client_socket.close()
+        
 
 def main():
     parseArgs()
@@ -208,7 +249,7 @@ def main():
     
     
     cleanupSockets()
-    print("Debug: Succesfully cleaned up connections to ts1 and ts2")
+    print("Debug: Succesfully cleaned up sockets")
     
 
 if (__name__ == "__main__"):
